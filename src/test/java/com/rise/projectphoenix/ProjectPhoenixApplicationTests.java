d0649a5aa3165e4a597fc949c40d6daf3a9a50fb
joeperez11 <perez.joseph.k@gmail.com>
2022-08-18 23:03:46 -0500

    first commit

---
+++ src/test/java/com/rise/projectphoenix/ProjectPhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rise.projectphoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
